c09780b9b85e4eb17fba153aa81200fe50be9494
sarveshwarbansal <sarvesh.bansal.1996@gmail.com>
2023-12-07 11:49:49 +0530

    changes made

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -6,12 +5,3 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 87a7c2f76af5744ab96dcaa4fa8ccd0185774873
