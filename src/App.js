d6811da2a0e63dfb336d26bcb861fb184fabaca5
sarveshwarbansal <104439314+sarveshwarbansal@users.noreply.github.com>
2023-12-07 13:56:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 import {
@@ -30,36 +30,3 @@ const App = () => (
 );
 
 export default App;
-=======
-import React from "react";
-
-import {
-  Footer,
-  Blog,
-  Possibility,
-  Features,
-  WhatGPT3,
-  Header,
-} from "./containers";
-import { CTA, Brand, Navbar } from "./components";
-
-import "./App.css";
-
-const App = () => (
-  <div className="App">
-    <div className="gradient__bg">
-      <Navbar />
-      <Header />
-    </div>
-    <Brand />
-    <WhatGPT3 />
-    <Features />
-    <Possibility />
-    <CTA />
-    <Blog />
-    <Footer />
-  </div>
-);
-
-export default App;
->>>>>>> 87a7c2f76af5744ab96dcaa4fa8ccd0185774873
